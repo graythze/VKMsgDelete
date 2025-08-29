e5c27ba6b1e026ccc2ef944d2e00dea45da3a389
graythze <54765502+graythze@users.noreply.github.com>
2025-08-30 00:34:35 +0500

    Fresh msg_delete.py

--- msg_delete.py
+++ msg_delete.py
@@ -1,19 +1,37 @@
 import requests
 import time
+import argparse
 
-min_msg = 1
-max_msg = 15000000
+# Config values. Probably no need to tweak
+bulk_message_size = 100
+api_timeout = 0.34
 
-vk_token = ""
 
-for i in range(min_msg, max_msg, 100):
-    msgs = ",".join(map(str, range(i, min(i + 100, max_msg))))
-    print(msgs)
+def delete_messages(min_msg: int, max_msg: int, vk_token: str):
+    for i in range(min_msg, max_msg, bulk_message_size):
+        msgs = ",".join(map(str, range(i, min(i + bulk_message_size, max_msg))))
+        print(f"Deleting messages: {msgs}")
 
-    messages = requests.get(f"https://api.vk.com/method/messages.delete",
-                    params={"message_ids": msgs,
-                          "delete_for_all": 1,
-                          "v": "5.199",
-                          "access_token": vk_token})
-    print(messages.json())
-    time.sleep(0.34)
+        response = requests.get(
+            "https://api.vk.com/method/messages.delete",
+            params={
+                "message_ids": msgs,
+                "delete_for_all": 1,
+                "v": "5.199",
+                "access_token": vk_token
+            }
+        )
+
+        print(response.json())
+        time.sleep(api_timeout)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="VK Message Remover")
+    parser.add_argument("--min_msg", type=int, required=True, help="Start message ID")
+    parser.add_argument("--max_msg", type=int, required=True, help="End message ID")
+    parser.add_argument("--vk_token", type=str, required=True, help="VK Token")
+
+    args = parser.parse_args()
+
+    delete_messages(args.min_msg, args.max_msg, args.vk_token)
